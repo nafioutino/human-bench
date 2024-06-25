da18486d0192ddc6c6a0c0e5b987cbd2f701ebcc
Abdoul-Nafiou <nafiou531@gmail.com>
2024-06-25 16:05:57 +0100

    la navigation ajoutée

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,15 @@ import gameArea from "@/components/gameArea.vue";
 
 <template>
   <div>
-    <nav>
-      <router-link to="/">HUMAN BENCHMARK</router-link>
-      <router-link to="/">DASHBOARD</router-link>
-      <router-link to="/">SIGN UP</router-link>
-      <router-link to="/">LOGIN</router-link>
+    <nav class="barre">
+      <div class="rl1">
+        <router-link to="/">HUMAN BENCHMARK</router-link>
+        <router-link to="/">DASHBOARD</router-link>
+      </div>
+      <div class="rl2">
+        <router-link to="/">SIGN UP</router-link>
+        <router-link to="/">LOGIN</router-link>
+      </div>
     </nav>
   </div>
   <gameArea />
@@ -17,5 +21,28 @@ import gameArea from "@/components/gameArea.vue";
 </template>
 
 <style scoped>
+.barre {
+  display: flex;
+  gap: 20%;
+}
+
+.rl1 {
+  display: flex;
+  gap: 2%;
+  margin-left: 20%;
+}
+
+.rl2 {
+  display: flex;
+  gap: 5%;
+  margin-right: 20%;
+}
+
+
+a {
+ color: black;
+text-decoration: none;
+
+}
 
 </style>
